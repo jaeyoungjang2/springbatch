d9115ebbf0236062ff2bcfa77ea898007738a041
jaeyoungjang2 <lion85488@gmail.com>
2023-01-09 22:47:01 +0900

    feat: #8 jobBuilderFactory, jobBuilder, Flowjob  - flowjob을 생성하는 방법

--- src/main/java/io/springbatch/springbatch/ExecutionContextConfiguration.java
+++ src/main/java/io/springbatch/springbatch/ExecutionContextConfiguration.java
@@ -5,8 +5,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,16 +26,24 @@ public class ExecutionContextConfiguration {
 
     private final JobExecutionListener jobRepositoryListener;
 
-    @Bean
-    public Job testtest() {
+    /*@Bean
+    public Job batchJob1() {
         return jobBuilderFactory.get("batchJob1")
                 .start(step1())
                 .next(step2())
                 .listener(jobRepositoryListener)
                 .build();
+    }*/
+    @Bean
+    public Job batchJob2() {
+        return jobBuilderFactory.get("batchJob1")
+                .start(flow())
+                .next(step5())
+                .end()
+                .build();
     }
 
-    @Bean
+    /*@Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
                 .tasklet(executionContextTasklet1)
@@ -40,17 +54,46 @@ public class ExecutionContextConfiguration {
         return stepBuilderFactory.get("step2")
                 .tasklet(executionContextTasklet2)
                 .build();
+    }*/
+
+    @Bean
+    public Flow flow() {
+        // step과 비슷한 level을 flow라고 봐도 무방할 것 같다.
+        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
+        // flow는 end를 구성해야한다.
+        flowBuilder.start(step3())
+                .next(step4())
+                .end();
+
+        return flowBuilder.build();
     }
-//    @Bean
-//    public Step step3() {
-//        return stepBuilderFactory.get("step3")
-//                .tasklet(executionContextTasklet3)
-//                .build();
-//    }
-//    @Bean
-//    public Step step4() {
-//        return stepBuilderFactory.get("step4")
-//                .tasklet(executionContextTasklet4)
-//                .build();
-//    }
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("step3 was executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step4() {
+        return stepBuilderFactory.get("step4")
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("step3 was executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step step5() {
+        return stepBuilderFactory.get("step3")
+                .tasklet((stepContribution, chunkContext) -> {
+                    System.out.println("step5 was executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
 }
