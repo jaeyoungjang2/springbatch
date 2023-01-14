e6a5f92ebd9f6d2151e23f0f96b57fc96c4dba65
jaeyoungjang2 <lion85488@gmail.com>
2023-01-14 15:42:57 +0900

    feat #9 validator  - DefaultJobParametersValidator 사용

--- src/main/java/io/springbatch/springbatch/ExecutionContextConfiguration.java
+++ src/main/java/io/springbatch/springbatch/ExecutionContextConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.scope.context.ChunkContext;
@@ -29,7 +30,7 @@ public class ExecutionContextConfiguration {
         return jobBuilderFactory.get("batchJob1")
                 .start(step1())
                 .next(step2())
-                .validator(new CustomJobParametersvalidator())
+                .validator(new DefaultJobParametersValidator(new String[]{"name","date"}, new String[]{"count"}))
                 .listener(jobRepositoryListener)
                 .build();
     }
