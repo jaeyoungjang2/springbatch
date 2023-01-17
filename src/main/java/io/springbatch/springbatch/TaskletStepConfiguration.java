fe1eef5d73246be2a3a039565eb7b1c4697c2c3c
jaeyoungjang2 <lion85488@gmail.com>
2023-01-17 21:59:13 +0900

    feat #5 step  - chunk step, flow step

---
+++ src/main/java/io/springbatch/springbatch/TaskletStepConfiguration.java
@@ -0,0 +1,58 @@
+package io.springbatch.springbatch;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@RequiredArgsConstructor
+@Configuration
+public class TaskletStepConfiguration {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job batchJob() {
+        return this.jobBuilderFactory.get("batchJob")
+                .start(taskStep())
+                .next(chunkStep())
+                .build();
+    }
+
+    // 단일 task 타입의 process
+    @Bean
+    public Step taskStep() {
+        return stepBuilderFactory.get("taskStep")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step1 has executed");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+    @Bean
+    public Step chunkStep() {
+        return stepBuilderFactory.get("chunkStep")
+                .<String, String>chunk(3)
+                .reader(new ListItemReader(Arrays.asList("item1","item2","item3")))
+                .processor(new ItemProcessor<String, String>() {
+                    @Override
+                    public String process(String item) throws Exception {
+                        return item.toUpperCase();
+                    }
+                })
+                .writer(list -> {
+                    list.forEach(item -> System.out.println(item));
+                })
+                .build();
+    }
+
+}
